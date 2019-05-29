f5d2cdbbbfaf6294d3c522c90d8812277503c334
wrs <wrs@cin.ufpe.br>
2019-05-29 11:53:04 -0300

    RepositorioPessoasArray - métodos inseridos RepositorioPessoas - atributo de remover p/ String

--- Sebonet/src/Repositorios/RepositorioPessoas.java
+++ Sebonet/src/Repositorios/RepositorioPessoas.java
@@ -4,7 +4,7 @@ import ClassesBasicas.Pessoas;
 public interface RepositorioPessoas {
     public void inserir(Pessoas pessoa);
     public void atualizar(Pessoas pessoa); // Criar a exceção
-    public void remover(Pessoas pessoa); // Criar a exceção
+    public void remover(String nome); // Criar a exceção
     public Pessoas procurar(String nome); // Criar a exceção
     public boolean existe(String nome); // Criar a exceção
 }
