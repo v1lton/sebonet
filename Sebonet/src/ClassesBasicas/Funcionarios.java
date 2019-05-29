0a8cf892952b9a344c7d61861f22b9b7f746bcec
wrs <wrs@cin.ufpe.br>
2019-05-29 14:33:03 -0300

    Alterações em Funcionarios

--- Sebonet/src/ClassesBasicas/Funcionarios.java
+++ Sebonet/src/ClassesBasicas/Funcionarios.java
@@ -3,19 +3,20 @@ package ClassesBasicas;
 public class Funcionarios extends Pessoas {
     private String cargo;
     private double salario;
-    // private Lojas loja;
+    private int loja;
 
-    public Funcionarios ( String nome, String CPF, String numero, String email, double credito, String cargo, double salario) {
+    public Funcionarios ( String nome, String CPF, String numero, String email, double credito, String cargo, double salario, int loja) {
         super (nome, CPF, numero, email, credito);
         this.cargo = cargo;
         this.salario = salario;
+        this.loja = loja;
     }
 
     public String getCargo() {
         return this.cargo;
     }
 
-    public void setCargo(String cargo) { // Definir quais são os cargos e aumentar os salarios atraves do cargo
+    public void setCargo(String cargo) {
         this.cargo = cargo;
     }
 
@@ -27,7 +28,15 @@ public class Funcionarios extends Pessoas {
         this.salario = salario;
     }
 
-    public boolean comprar(String nomeLivro) {
+    public int getLoja() {
+        return this.loja;
+    }
+
+    public void setLoja(int loja) {
+        this.loja = loja;
+    }
+
+    public boolean comprar(String livro) {
         return true;
     }
 }
