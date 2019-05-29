29f777dc0c396f41d162076b306e2f4b932e0ebf
Paulo Matheus Santiago dos Santos Melo <pmssm@ug3c48.windows.cin.ufpe.br>
2019-05-29 11:58:51 -0300

    repositorios lojas

--- Sebonet/src/Repositorios/RepositorioLojas.java
+++ Sebonet/src/Repositorios/RepositorioLojas.java
@@ -1,4 +1,17 @@
 package Repositorios;
+import ClassesBasicas.Lojas;
 
-public class RepositorioLojas {
+public interface RepositorioLojas {
+
+    void inserir(Lojas loja);
+
+
+    void remover(int id);
+
+
+    void atualizar (Lojas loja);
+
+    boolean existe (int id);
+
+    Lojas procurar (int id);
 }
--- Sebonet/src/Repositorios/RepositorioLojasArray.java
+++ Sebonet/src/Repositorios/RepositorioLojasArray.java
@@ -1,4 +1,62 @@
 package Repositorios;
 
-public class RepositorioLojasArray {
+import ClassesBasicas.Lojas;
+
+public class RepositorioLojasArray  implements  RepositorioLojas {
+    private Lojas[] lojas;
+    private int indice;
+
+    public RepositorioLojasArray(){
+        this.lojas = new Lojas[50];
+        this.indice = 0;
+    }
+
+   public void inserir(Lojas loja){
+        if(this.indice < this.lojas.length){
+            lojas[this.indice] = loja;
+        }else{
+            //erro
+        }
+    }
+
+    public void remover(int id){
+
+        if(this.existe(id)){
+            for(int i = 0; i < this.lojas.length; i++){
+                if(this.lojas[i].getId() == id){
+                    this.lojas[i] = null;
+                }
+            }
+
+            //organizando a lista
+            for(int i = 0; i < this.lojas.length-1; i++){
+                if(this.lojas[i+1] != null && this.lojas[i] == null){
+                    this.lojas[i] = this.lojas[i+1];
+                    this.lojas[i+1] = null;
+                }
+            }
+
+        }else{
+            //erro
+        }
+    }
+
+    public void atualizar (Lojas loja){
+
+    }
+
+    public boolean existe (int id){
+
+        for(int i = 0; i < this.lojas.length; i++){
+            if(this.lojas[i].getId() == id){
+                return true;
+            }
+        }
+       return false;
+    }
+
+    public Lojas procurar (int id){
+
+
+    }
 }
