fa991059fcfaf6b303b6e2ba1dccce9a746a84ae
wrs <wrs@cin.ufpe.br>
2019-05-31 08:40:02 -0300

    Preenchimento de CadastroPessoas

--- Sebonet/src/ClassesFachadaIndividual/CadastroPessoas.java
+++ Sebonet/src/ClassesFachadaIndividual/CadastroPessoas.java
@@ -1,4 +1,48 @@
 package ClassesFachadaIndividual;
 
+import Excecoes.*;
+import Repositorios.*;
+import ClassesBasicas.*;
+
 public class CadastroPessoas {
+    private RepositorioPessoas rep;
+
+    public CadastroPessoas(RepositorioPessoas rep) {
+        this.rep = rep;
+    }
+
+    public void cadastrar(Pessoas pessoa) throws LimitePessoasException, PessoaNaoEncontradaException{
+        if (this.existe(pessoa.getCPF())) {
+            rep.inserir(pessoa);
+        } else {
+            throw new PessoaNaoEncontradaException();
+        }
+
+    }
+
+    public void atualizar (Pessoas pessoa) throws PessoaNaoEncontradaException{
+        if (this.existe(pessoa.getCPF())) {
+            rep.atualizar(pessoa);
+        } else {
+            throw new PessoaNaoEncontradaException();
+        }
+    }
+
+    public void remover (String CPF) throws PessoaNaoEncontradaException{
+        if (this.existe(CPF)) {
+            rep.remover(CPF);
+        } else {
+            throw new PessoaNaoEncontradaException();
+        }
+    }
+
+    public boolean existe(String CPF) {
+        return rep.existe(CPF);
+    }
+
+    public Pessoas procurar(String CPF) throws PessoaNaoEncontradaException {
+        return rep.procurar(CPF);
+    }
+
+
 }
