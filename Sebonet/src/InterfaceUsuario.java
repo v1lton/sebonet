c17115711c5913290bdebf7cf5fbdbd3f48d2c73
v1lton <wrs@cin.ufpe.br>
2019-06-07 13:58:01 -0300

    a

--- Sebonet/src/InterfaceUsuario.java
+++ Sebonet/src/InterfaceUsuario.java
@@ -5,7 +5,7 @@ import Excecoes.LojaNaoEncontradaException;
 public class InterfaceUsuario {
 
     public static void main(String[] args) throws LimiteLojaException, LojaNaoEncontradaException, LojaJaCadastradaException {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
         FachadaGeral.TestesLojas.main();
     }
 
