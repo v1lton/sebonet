909dc58f21de42e6d622fc87d3d4303e35f5c2c4
v1lton <wrs@cin.ufpe.br>
2019-06-08 20:54:42 -0300

    alteração na UI

--- Sebonet/src/InterfaceUsuario.java
+++ Sebonet/src/InterfaceUsuario.java
@@ -1,10 +1,19 @@
+import ClassesBasicas.*;
 import Excecoes.*;
+import Repositorios.*;
+import FachadaGeral.*;
+import ClassesFachadaIndividual.*;
 
 public class InterfaceUsuario {
 
-    public static void main(String[] args) throws PessoaNaoEncontradaException, PessoaCadastradaException, CargoNaoExistenteException, LimitePessoasException, SaldoInsuficienteException{
-        System.out.println("Hello World!!");
-        FachadaGeral.TestePessoas.main();
+    public static void main(String[] args){
+        RepositorioPessoas repPessoas = new RepositorioPessoasArray(5);
+        RepositorioLojas repLojas = new RepositorioLojasArray();
+        RepositorioLivros repLivros = new RepositorioLivroArray(2);
+        CadastroPessoas crudPessoas = new CadastroPessoas(repPessoas);
+        CrudLojas crudLojas = new CrudLojas(repLojas);
+        CadastroLivro crudLivros = new CadastroLivro(repLivros);
+        Sebo sebo = new Sebo(crudPessoas, crudLojas, crudLivros);
     }
 
 }
