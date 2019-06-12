4fe97c4c57732346b535f417ffcbe02740c848bf
ncm <ncm@cin.ufpe.br>
2019-06-12 11:04:05 -0300

    Interface Usuario

--- Sebonet/src/InterfaceUsuario.java
+++ Sebonet/src/InterfaceUsuario.java
@@ -12,7 +12,7 @@ public class InterfaceUsuario {
         RepositorioLivros repLivros = new RepositorioLivroArray(2);
         CadastroPessoas crudPessoas = new CadastroPessoas(repPessoas);
         CrudLojas crudLojas = new CrudLojas(repLojas);
-        CadastroLivro crudLivros = new CadastroLivro(repLivros);
+        CadastroLivros crudLivros = new CadastroLivros(repLivros);
         Sebo sebo = new Sebo(crudPessoas, crudLojas, crudLivros);
         TesteLivro.main();
         try {
