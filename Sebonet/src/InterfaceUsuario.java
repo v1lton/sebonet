4666170fdd06606d3a4bf49b127f2fe1b323a797
v1lton <wrs@cin.ufpe.br>
2019-06-12 11:50:27 -0300

    UI

--- Sebonet/src/InterfaceUsuario.java
+++ Sebonet/src/InterfaceUsuario.java
@@ -16,8 +16,32 @@ public class InterfaceUsuario {
         Sebo sebo = new Sebo(crudPessoas, crudLojas, crudLivros);
         TesteLivro.main();
         try {
-            sebo.cadastroPessoas(new Funcionarios("Jose", "11897419465", "81996571720", "js@cin.ufpe.br", 200, "Gerente", 10));
-        } catch (Exception e) {
+            sebo.cadastroPessoas(new Funcionarios("Jose", "11897419465", "81996571720", "js@cin.ufpe.br", 200, "Gerente", 01));
+        } catch (PessoaJaCadastradaException e) {
+            System.out.println(e.getMessage());
+        } catch (CargoNaoExistenteException e) {
+            System.out.println(e.getMessage());
+        } catch (LojaNaoEncontradaException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            sebo.cadastroPessoas(new Funcionarios("Artur", "2212212213", "888885", "edurom@gmail.com", 100, "Vendedor", 01));
+            sebo.cadastroPessoas(new Funcionarios("Artur", "2212212213", "888885", "edurom@gmail.com", 100, "Vendedor", 01));
+        } catch (PessoaJaCadastradaException e) {
+            System.out.println(e.getMessage());
+        } catch (CargoNaoExistenteException e) {
+            System.out.println(e.getMessage());
+        } catch (LojaNaoEncontradaException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            sebo.removerPessoas("2212212214");
+        } catch (PessoaNaoEncontradaException e){
+            System.out.println(e.getMessage());
+        }
+        try {
+            sebo.removerPessoas("2212212213");
+        } catch (PessoaNaoEncontradaException e){
             System.out.println(e.getMessage());
         }
     }
